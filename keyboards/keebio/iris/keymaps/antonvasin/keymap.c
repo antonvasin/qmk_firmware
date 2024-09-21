06dd79cd57b1bd2efe7c3e6e7eb8053cdeaeb418
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-09-21 15:20:09 +0100

    remove shortcut defines

--- keyboards/keebio/iris/keymaps/antonvasin/keymap.c
+++ keyboards/keebio/iris/keymaps/antonvasin/keymap.c
@@ -3,11 +3,6 @@
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 3
-
-#define CTL_ESC LCTL_T(KC_ESC)
-#define LOWER TT(1)
-#define RAISE MO(2)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -17,11 +12,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 //     ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN, KC_QUOT,
+ LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN, KC_QUOT,
 //     ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,            KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
 //     └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                       KC_LALT, KC_LGUI, KC_SPC,                    KC_ENT,  LOWER,   RAISE
+                                       KC_LALT, KC_LGUI, KC_SPC,                    KC_ENT,  TT(1),   MO(2)
 //                                    └────────┴────────┴────────┘                 └────────┴────────┴────────┘
       ),
 
