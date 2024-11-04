23bdb945bd653bbd5fb22bd01ead0e2cec5cb708
Anton Vasin <944260+antonvasin@users.noreply.github.com>
2024-11-04 14:06:37 +0000

    left shift without cmd-space

--- keyboards/keebio/iris/keymaps/antonvasin/keymap.c
+++ keyboards/keebio/iris/keymaps/antonvasin/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     ├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
  LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,     KC_L,   KC_SCLN, KC_QUOT,
 //     ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-        LSFT_T(G(KC_SPC)), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,            KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_MINS),
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,            KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_MINS),
 //     └────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                        KC_LALT, KC_LGUI, KC_SPC,                    KC_ENT,  TT(1),   MO(2)
 //                                    └────────┴────────┴────────┘                 └────────┴────────┴────────┘
